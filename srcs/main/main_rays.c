42c2b061eecb8c39665f769ab665ba924e92f0a2
Andrew James Meechan <ameechan@student.42lausanne.ch>
2025-03-18 16:26:53 +0100

    commented mains

--- srcs/main/main_rays.c
+++ srcs/main/main_rays.c
@@ -12,14 +12,14 @@
 
 #include "../../includes/minirt.h"
 
-// int	main()
-// {
-// 	if (position_test(0))//	0 to skip, 1 to run
-// 		printf(R_B"Error:"RES" position_test failed\n");
-// 	if (sph_unique_id(0, 10))// 0 to skip, 1 to run
-// 		printf(R_B"Error:"RES" sph_unique_id failed\n");
-// 	if (sph_ray_intersect(1))// 0 to skip, 1 to run
-// 		printf(R_B"Error:"RES" sph_ray_intersect failed\n");
-// 	printf(R_B"END OF RAYS TESTS\n"RES);
-// 	return (0);
-// }
+int	main()
+{
+	if (position_test(0))//	0 to skip, 1 to run
+		printf(R_B"Error:"RES" position_test failed\n");
+	if (sph_unique_id(0, 10))// 0 to skip, 1 to run
+		printf(R_B"Error:"RES" sph_unique_id failed\n");
+	if (sph_ray_intersect(1))// 0 to skip, 1 to run
+		printf(R_B"Error:"RES" sph_ray_intersect failed\n");
+	printf(R_B"END OF RAYS TESTS\n"RES);
+	return (0);
+}
