4b92aff024432ce55ad2d8f74c333c02dda74812
Mikan95 <andrewmeechan@hotmail.com>
2025-03-31 19:42:09 +0200

    commented main

--- srcs/main/main_phong.c
+++ srcs/main/main_phong.c
@@ -12,16 +12,16 @@
 
 #include "../../includes/minirt.h"
 
-// int	main(void)
-// {
-// 	if (normal_at_test(0)) //1 to run , 0 to skip
-// 		printf("Error: normal_at_test failed\n");
-// 	if (light_test(0)) //1 to run , 0 to skip
-// 		printf("Error: light_test failed\n");
-// 	if (material_test(0)) //1 to run , 0 to skip
-// 		printf("Error: material_test failed\n");
-// 	if (shading_test(1)) //1 to run , 0 to skip
-// 		printf("Error: shading_test failed\n");
-// 	printf(R_B"END OF REFLECTION TESTS\n"RES);
-// 	return (0);
-// }
+int	main(void)
+{
+	if (normal_at_test(0)) //1 to run , 0 to skip
+		printf("Error: normal_at_test failed\n");
+	if (light_test(0)) //1 to run , 0 to skip
+		printf("Error: light_test failed\n");
+	if (material_test(0)) //1 to run , 0 to skip
+		printf("Error: material_test failed\n");
+	if (shading_test(1)) //1 to run , 0 to skip
+		printf("Error: shading_test failed\n");
+	printf(R_B"END OF REFLECTION TESTS\n"RES);
+	return (0);
+}
