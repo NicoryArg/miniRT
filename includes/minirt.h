8cda627b632a08bca0b2430c8d596c21dcd4354f
Andrew James Meechan <ameechan@student.42lausanne.ch>
2025-04-18 16:21:17 +0200

    added planes and draw_planes scene

--- includes/minirt.h
+++ includes/minirt.h
@@ -441,7 +441,7 @@ t_matrix	*view_transform(t_tuple from, t_tuple to, t_tuple up);
 # define EPSILON 0.00001
 
 // Define window and view parameters
-# define WIN_SIZE 1500
+# define WIN_SIZE 300
 # define VIEW_CHANGE_SIZE 60
 # define MIN_ITERATIONS 256
 # define MAX_ITERATIONS 256
