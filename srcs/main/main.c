302b3b0cd23604dd56f2dc0f07dbfaa4c9047f67
Mikan95 <andrewmeechan@hotmail.com>
2025-04-19 03:29:18 +0200

    changed render

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -30,9 +30,9 @@ int	ft_main_render(int run)
 		return (printf(AKA"❌ draw_world failed\n"RES));
 	if (ft_main_draw_world_shadows(0))
 		return (printf(AKA"❌ draw_world failed\n"RES));
-	if (ft_main_draw_patterns(0))
+	if (ft_main_draw_patterns(1))
 		return (printf(AKA"❌ draw_patterns failed\n"RES));
-	if (ft_main_draw_world_planes(1))
+	if (ft_main_draw_world_planes(0))
 		return (printf(AKA"❌ draw_world failed\n"RES));
 	printf(G_B"END OF RENDER DEMOS\n"RES);
 	return (0);
