64e2759f5ac2a28c0b1ef1eb1c3ea2b6efa1c0a7
Andrew James Meechan <ameechan@student.42lausanne.ch>
2025-04-28 17:46:18 +0200

    parse_lines

--- srcs/parse/check_input.c
+++ srcs/parse/check_input.c
@@ -6,7 +6,7 @@
 /*   By: ameechan <ameechan@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 18:42:06 by ameechan          #+#    #+#             */
-/*   Updated: 2025/04/28 16:14:16 by ameechan         ###   ########.fr       */
+/*   Updated: 2025/04/28 17:36:09 by ameechan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@ int	check_input(char *file)
 
 	if (grab_input(file, &input))
 		return (-1);
+	if ()
 	printf("%s", input);
 	return (0);
 }
